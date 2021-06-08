51139db75c5ad19cca23b2d5bde2836f272f9fcf
Franlly Mendoza Fana <franlly_mendoza@ucne.edu.do>
2021-06-07 23:47:34 -0400

    agregando algunos ejemplos del tutorial

---
+++ Models/Courses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial.Models
+{
+    
+    public class Courses
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public IList<StudentCourses> StudentCourses { get; set; }
+    }
+}
---
+++ Models/StudentCourses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial.Models
+{
+    
+    public class StudentCourses
+    {
+        public int StudentId { get; set; }
+        public Students Student { get; set; }
+
+        public int CourseId { get; set; }
+        public Courses Course { get; set; }
+    }
+}
